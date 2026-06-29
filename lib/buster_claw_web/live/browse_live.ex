162f43bce4b86fba4e16ff93e2c89819049c5afb
Luke Hightower <lukehightower11@gmail.com>
2026-06-28 19:58:00 -0700

    feat(browser): /browse fills window width (full_bleed)

--- lib/buster_claw_web/live/browse_live.ex
+++ lib/buster_claw_web/live/browse_live.ex
@@ -37,7 +37,7 @@ defmodule BusterClawWeb.BrowseLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash}>
+    <Layouts.app flash={@flash} full_bleed>
       <section
         id={"browse-shell-" <> @surface_id}
         phx-hook="EmbeddedBrowser"
